28fc15cb28f28bc6c4ba2564b2d58de6a06244b7
mgarbula <mgarbula@student.agh.edu.pl>
2024-08-29 16:02:44 +0200

    Main class

---
+++ src/main/java/com/example/chat_project/ChatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatProjectApplication.class, args);
+	}
+
+}
